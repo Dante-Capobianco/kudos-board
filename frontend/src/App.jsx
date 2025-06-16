bcf6bc5bc40a5878072c5316a7623871c898ec2d
Dante Nicola Capobianco <dante.capobianco17@gmail.com>
2025-06-15 21:25:17 -0700

    Implement header styling with search bar, submit/clear buttons, sidebar icon, and title

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,86 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const titleIconSrc = "/kudos.png";
+  const titleIconAlt = "Kudos medal icon";
+  const titleIconSize = "40px";
+  const submit = "submit";
+
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const [searchQueryToSubmit, setSearchQueryToSubmit] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    document.getElementById("submit-search-btn").blur();
+    setSearchQueryToSubmit(searchValue);
+  };
+
+  const handleClear = (event) => {
+    event.preventDefault();
+    document.getElementById("clear-search-btn").blur();
+    setSearchValue("");
+    setSearchQueryToSubmit("");
+  };
+
   return (
-    <div>
-      <header></header>
+    <div className="app">
+      <header>
+        <h1>
+          <img
+            className="header-img"
+            src={titleIconSrc}
+            alt={titleIconAlt}
+            width={titleIconSize}
+          />
+          Kudos Board
+          <img
+            className="header-img"
+            src={titleIconSrc}
+            alt={titleIconAlt}
+            width={titleIconSize}
+          />
+        </h1>
+        <p style={{ fontFamily: "fantasy" }}>
+          Give a kudos, get a kudos... kudos all around!
+        </p>
+
+        <section className="search-sort-container">
+          <span
+            class="sidebar-icon material-symbols-outlined"
+            onClick={() => setIsSideBarOpen(!isSideBarOpen)}
+          >
+            {isSideBarOpen ? "density_medium" : "menu"}
+          </span>
+          <form>
+            <input
+              type="text"
+              class="search-bar"
+              placeholder="Search for a board by title"
+              value={searchValue}
+              onChange={(event) => setSearchValue(event.target.value)}
+            />
+            <button
+              type={submit}
+              onClick={(event) => handleSubmit(event)}
+              id="submit-search-btn"
+              className="search-btn"
+            >
+              Search
+            </button>
+            <button
+              type={submit}
+              onClick={(event) => handleClear(event)}
+              id="clear-search-btn"
+              className="search-btn"
+            >
+              Clear
+            </button>
+          </form>
+        </section>
+      </header>
       <main></main>
-      <footer></footer>
     </div>
   );
 }
